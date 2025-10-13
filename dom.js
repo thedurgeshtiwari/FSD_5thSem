f8704c85b7662be702b786a6f0313e4559687419
XYZthedurgeshtiwari <tdurgesh448@gmail.com>
2025-10-13 12:03:12 +0530

    physical dom

---
+++ dom.js
@@ -0,0 +1,6 @@
+const div=document.createElement("div");
+const h1=document.createElement("h1");
+h1.innerHTML="hi we are python";
+div.appendChild(h1);
+const id=document.getElementById("root");
+id.appendChild(div);``
