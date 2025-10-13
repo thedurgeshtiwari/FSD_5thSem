f308436185d5f30fdde7d38538f7eeb4841f5676
XYZthedurgeshtiwari <tdurgesh448@gmail.com>
2025-10-13 12:25:58 +0530

    virtual dom

--- dom.js
+++ dom.js
@@ -3,4 +3,4 @@ const h1=document.createElement("h1");
 h1.innerHTML="hi we are python";
 div.appendChild(h1);
 const id=document.getElementById("root");
-id.appendChild(div);``
+id.appendChild(div);
